82c26468acc37684c48505e67fbe7c2e011419f9
Sama-E <112719147+Sama-E@users.noreply.github.com>
2023-05-25 12:28:42 -0600

    map & initial marker working

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,35 @@
-import { useState } from 'react';
-import ReactMapGl from 'react-map-gl';
+import * as React from 'react';
+import Map, {Marker} from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
+import { Circle, Star, StarBorder } from '@mui/icons-material';
+
+const TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+
 function App() {
-  const [viewPort, setViewPort] = useState({
-    width: "100vw",
-    height: "100vh",
-    longitude: 37.7577,
-    latitude: -80,
-    zoom: 5
+  const [viewState, setViewState] = React.useState({
+    latitude: 39.742043,
+    longitude: -104.991531,
+    zoom: 8
   });
   return (
-    <div className="App">
-      <ReactMapGl
-          {...viewPort}
-          mapboxApiAccessToken = {process.env.REACT_APP_PUBLIC_MAPBOX}
-          onViewPortChange={nextViewPort => setViewPort(nextViewPort)}
-        />
-    </div>
+    <Map
+      {...viewState}
+      onMove={evt => setViewState(evt.viewState)}
+      style={{width: 800, height: 600}}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      mapboxAccessToken={TOKEN}
+    >
+      <Marker 
+        longitude={-104.96234} 
+        latitude={39.68356}
+        offsetLeft={-20}
+        offsetRight={-10} 
+      >
+        <Circle 
+          style={{fontSize:viewState.zoom*1, color:"red"}} />
+      </Marker>
+    </Map>
   );
 }
 
