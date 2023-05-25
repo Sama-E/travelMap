e85d5b81d8064c32604d98898d4fc6e27cc63353
Sama-E <112719147+Sama-E@users.noreply.github.com>
2023-05-24 19:27:27 -0600

    server, pins

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const pinRoute = require("./routes/pins");
+
+const app = express();
+
+dotenv.config();
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+  })
+  .then(() => {
+    console.log("MongoDB Connected!")
+  })
+  .catch((err) => console.log(err));
+
+//Routes
+app.use("/api/pins", pinRoute);
+
+app.listen(8800, () => {
+  console.log("Server running!");
+})
